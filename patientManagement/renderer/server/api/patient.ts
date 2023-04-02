31bc01d8543fc7ef39cc4c9993d17fa4deef0c54
Johndel <johndelencabo@gmail.com>
2023-04-02 11:23:39 +0800

    change back to default

--- patientManagement/renderer/server/api/patient.ts
+++
@@ -1,7 +0,0 @@
-import { useQuery } from "@tanstack/react-query"
-import { API_URL } from ".."
-import { Patient } from "../../schema/patient"
-import { getPatientDetail, getPatientOverview } from "../request"
-
-
-
